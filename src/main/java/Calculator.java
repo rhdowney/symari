0d1057725e509445751911aa4ad05429d0e8312d
Rich Downey <rich.downey@jhuapl.edu>
2025-10-05 22:00:11 -0400

    Calculator and CalculatorTest example unit test

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,15 @@
+/* This is a sample class that passes the sample tests in 
+   CalculatorTest.java.
+   author: Rich Downey
+   */
+class Calculator {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int subtract(int a, int b) {
+        return a - b;
+    }
+
+}
